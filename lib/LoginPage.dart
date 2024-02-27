2f317542555ad91b469623b3d0907a4a2d837caa
AtanuPanja <atanupanja9463@gmail.com>
2024-02-27 09:50:53 +0530

    Changes in LoginPage
    Made the LoginPage scrollable to counter
    size changes when keyboard appears
    
    Made the keyboard type as numberpad

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -126,174 +126,177 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  child: Icon(Icons.close),
-                ),
-                Expanded(
-                    child: Text(
-                  'Login or Signup',
-                  textAlign: TextAlign.center,
-                )),
-              ],
-            ),
-            Divider(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  child: Icon(Icons.person),
-                ),
-                Text('Welcome to HoldInn'),
-                Container(
-                  padding: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey.shade700,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    child: Icon(Icons.close),
                   ),
-                  child: Column(
+                  Expanded(
+                      child: Text(
+                    'Login or Signup',
+                    textAlign: TextAlign.center,
+                  )),
+                ],
+              ),
+              Divider(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    child: Icon(Icons.person),
+                  ),
+                  Text('Welcome to HoldInn'),
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey.shade700,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Country/Region',
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.grey.shade700,
+                                      ),
+                                    ),
+                                    Text('India (+91)'),
+                                  ],
+                                ),
+                              ),
+                              InkWell(
+                                child: Icon(Icons.keyboard_arrow_down),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(),
+                        Container(
+                          child: TextField(
+                            controller: phoneController,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                              hintText: 'Phone number',
+                              border: InputBorder.none,
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "We'll call or text to confirm your number, standard message and data rates apply.",
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.grey.shade700,
+                          fontSize: 12,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text:
+                                'By signing in to this app, you agree to the HoldInn ',
+                          ),
+                          TextSpan(
+                            style: TextStyle(
+                              color: Colors.blue,
+                            ),
+                            text: 'Terms & Conditions ',
+                          ),
+                          TextSpan(
+                            text: 'and acknowledge the ',
+                          ),
+                          TextSpan(
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                              text: 'Privacy Policy.'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: continueButtonCallback,
+                    child: Text('Continue'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Country/Region',
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      color: Colors.grey.shade700,
-                                    ),
-                                  ),
-                                  Text('India (+91)'),
-                                ],
-                              ),
-                            ),
-                            InkWell(
-                              child: Icon(Icons.keyboard_arrow_down),
-                              onTap: () {},
-                            ),
-                          ],
+                        width: screenWidth / 2.5,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
                         ),
                       ),
-                      Divider(),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text('or'),
+                      SizedBox(
+                        width: 4,
+                      ),
                       Container(
-                        child: TextField(
-                          controller: phoneController,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            hintText: 'Phone number',
-                            border: InputBorder.none,
-                          ),
+                        width: screenWidth / 2.5,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
                         ),
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    "We'll call or text to confirm your number, standard message and data rates apply.",
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: RichText(
-                    text: TextSpan(
+                  InkWell(
+                    child: Text(
+                      'Show more options',
                       style: TextStyle(
-                        color: Colors.grey.shade700,
-                        fontSize: 12,
+                        decoration: TextDecoration.underline,
                       ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text:
-                              'By signing in to this app, you agree to the HoldInn ',
-                        ),
-                        TextSpan(
-                          style: TextStyle(
-                            color: Colors.blue,
-                          ),
-                          text: 'Terms & Conditions ',
-                        ),
-                        TextSpan(
-                          text: 'and acknowledge the ',
-                        ),
-                        TextSpan(
-                            style: TextStyle(
-                              color: Colors.blue,
-                            ),
-                            text: 'Privacy Policy.'),
-                      ],
                     ),
+                    onTap: () {},
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: continueButtonCallback,
-                  child: Text('Continue'),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    InkWell(
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                      ),
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: screenWidth / 2.5,
-                      height: 2,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Text('or'),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Container(
-                      width: screenWidth / 2.5,
-                      height: 2,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-                InkWell(
-                  child: Text(
-                    'Show more options',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
