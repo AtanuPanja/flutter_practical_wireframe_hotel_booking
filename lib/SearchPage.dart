3761271ccbadeeda53c90a2367d44b8d21b85387
AtanuPanja <atanupanja9463@gmail.com>
2024-02-27 15:05:14 +0530

    Designed the Search page
    Containing Near Me

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -10,18 +10,51 @@ class SearchPage extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                InkWell(
-                  child: Icon(Icons.arrow_back),
-                  onTap: () => Navigator.of(context).pop(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  InkWell(
+                    child: Icon(Icons.arrow_back),
+                    onTap: () => Navigator.of(context).pop(),
+                  ),
+                  Expanded(child: Text('Search')),
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  hintText: 'Search by city, landmark, or hotel',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade600,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
                 ),
-                Expanded(child: Text('Search')),
-              ],
-            )
-          ],
+              ),
+              ListTile(
+                leading: Icon(Icons.location_searching_outlined),
+                title: Text('Property Near Me'),
+                onTap: () {
+                  
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.location_on, color: Colors.grey.shade300,),
+                title: Text('Riyadh, Riyadh, Saudi Arabia'),
+                onTap: () {
+                  
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
